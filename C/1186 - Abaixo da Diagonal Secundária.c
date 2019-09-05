f5c0aceae1d733d9e88a67ae5a42c499ea63eb1f
Nádia Oliveira <nasilvah21@gmail.com>
2019-09-05 00:37:46 -0300

    Add more exercises
    corpo (72 caracteres)

--- C/1186 - Abaixo da Diagonal Secundária.c
+++ C/1186 - Abaixo da Diagonal Secundária.c
@@ -6,8 +6,6 @@ int main()
 	int i,j;
 	float sum = 0;
 	int count = 0;
-	int aux = 10;
-	int aux_mean = 0;
 	
 	scanf("%s",O);
 	for (i=0;i<12;i++)
@@ -15,26 +13,11 @@ int main()
 		for (j=0;j<12;j++)
 		{
 			scanf("%f",&M[i][j]);
-
-			/*Outra maneira de fazer
-			if (j <= aux)
-			{
-				sum += M[i][j];
-				count +=1;
-				if(count==aux+1)
-				{
-					aux = aux - 1;
-					aux_mean = aux_mean + count;
-					count = 0;
-				}
-			}*/
-			//Uma outra maneira de fazer
-			if (j<11-i)
+			if (j>11-i)
 			{
 				sum += M[i][j];
 				count +=1;
 			}
-			
 		}
 	}	
 	if (O[0] == 'S')
