56c3413d6a967b79564d16c149d5809d60f58e56
nadiaaoliverr <nasilvah21@gmail.com>
2020-01-02 09:45:43 -0300

    Add 1924,1930 em Python

---
+++ Python/1924 - Vitória e a Indecisão.py
@@ -0,0 +1,5 @@
+N = int(input())
+
+for i in range(N):
+    curso = str(input())
+print('Ciencia da Computacao')
---
+++ Python/1930 - Tomadas.py
@@ -0,0 +1,6 @@
+t1,t2,t3,t4 = input().split(' ')
+t1 = int(t1)
+t2 = int(t2)
+t3 = int(t3)
+t4 = int(t4)
+print((t1+t2+t3+t4)-3)
