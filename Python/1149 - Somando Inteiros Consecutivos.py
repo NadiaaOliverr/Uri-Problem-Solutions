fb883037fb84074955e35e383f838e2e98fd466c
Cleber Schroeder Fonseca <clebersfonseca@users.noreply.github.com>
2021-08-27 23:42:51 -0300

    Update 1149 - Somando Inteiros Consecutivos.py
    Esse algoritmo não solucionava o problema, agora sim está correto.

--- Python/1149 - Somando Inteiros Consecutivos.py
+++ Python/1149 - Somando Inteiros Consecutivos.py
@@ -1,15 +1,13 @@
-A,N = input().split(" ")
-A = int(A)
-N = int(N)
+X = input().split(" ")
+A = int(X.pop(0))
 
-
-while N<=0:
-    N = int(input())
-    
+for z in X:
+    if (int(z)>0):
+        N = int(z)
+        break
 
 soma = 0
-
 for i in range(N):
-   soma+=(A+i)
+    soma+=(A+i)
 
 print(soma)
